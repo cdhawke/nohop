b8a445432d17f2b2d9343e12eb414bdd854012ba
Chris Hawke <cdhawke1@gmail.com>
2022-02-26 08:11:30 -0500

    fix: es6

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-const debounce = (callback: (...args: unknown[]) => void, wait: number) => {
+const debounce = (func: (...args: unknown[]) => void, wait: number) => {
   let timeout: NodeJS.Timeout;
   return (...args: unknown[]) => {
     clearTimeout(timeout);
     timeout = setTimeout(() => {
-      callback.apply(this, args);
+      func(args);
     }, wait);
   };
 };
